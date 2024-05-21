31cabc1bc566079c6dbf9ca0d2882f8b1cfc3f82
brunoguerra <brunoguerra@hotmail.com>
2024-05-21 09:08:06 -0300

    feat: configuracoes iniciais

---
+++ src/main/java/br/com/fiap/finance/Request/CarteiraComAcoesRequest.java
@@ -0,0 +1,10 @@
+package br.com.fiap.finance.Request;
+
+import java.util.List;
+
+import br.com.fiap.finance.Model.Acao;
+
+public class CarteiraComAcoesRequest {
+    public String nomeCarteira;
+    public List<Acao> acoes;
+}
