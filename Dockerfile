0ae19ae1ab6b2189d7a918ebc5a55af11e8956ec
brunoguerra <brunoguerra@hotmail.com>
2024-05-24 09:15:36 -0300

    feat: docker e rota events

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+#Usando a imagem oficial do Maven para compilar o projeto Java
+FROM maven:3.8.4-openjdk-17-slim AS build
+
+# Definindo o diretório de trabalho dentro do container
+WORKDIR /app
+
+# Copiando o arquivo POM e os arquivos fonte do projeto
+COPY pom.xml .
+COPY src ./src
+
+# Compilando o projeto com Maven
+RUN mvn clean install -DskipTests
+
+#Usando uma imagem base JDK para executar a aplicação Java
+FROM openjdk:17-jdk-slim
+
+WORKDIR /app
+
+COPY --from=build /app/target/*.jar /app/msfinance.jar
+
+#CMD ["java", "-jar", "/app/msfinance.jar"]
+
+
+# Instalando wait-for-it.sh para poder esperar o  mongo
+RUN apt-get update -y \
+    && apt-get install -y wget netcat \
+    && wget -O /usr/local/bin/wait-for-it.sh https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh \
+    && chmod +x /usr/local/bin/wait-for-it.sh
+
+# Aguardando até que o MongoDB inicie antes de iniciar a aplicação
+CMD ["/bin/bash", "-c", "wait-for-it.sh -t 0 mongo-finance:27017 -- java -jar msfinance.jar"]
--- src/main/java/br/com/fiap/finance/Controller/CarteiraController.java
+++ src/main/java/br/com/fiap/finance/Controller/CarteiraController.java
@@ -1,6 +1,7 @@
 package br.com.fiap.finance.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,9 @@ import br.com.fiap.finance.Service.CarteiraService;
 
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+
+import java.time.Duration;
 
 
 @RestController
@@ -40,5 +44,13 @@ public class CarteiraController {
                 .map(totalAmount -> ResponseEntity.ok(totalAmount))
                 .defaultIfEmpty(ResponseEntity.notFound().build());
     }
+
+    @GetMapping(value = "/events", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Tuple2<Long, Carteira>> getCarteiraEvents() {
+        Flux<Long> interval = Flux.interval(Duration.ofSeconds(10));
+        Flux<Carteira> events = carteiraService.findAll();
+        return Flux.zip(interval, events);
+    }
+
    
 }
